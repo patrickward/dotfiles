50d74dc28cbfd6adb8de25815b552fe9cc9e2e97
Patrick <patrick@patrickward.com>
2018-10-14 08:21:11 -0400

    Adding jython start

--- zsh/python.zsh
+++ zsh/python.zsh
@@ -3,4 +3,4 @@
 alias workon='source activate'
 
 # set yourself up for using jython
-alias jython_start="export PATH=$HOME/jython2.7.0/bin:$PATH"
+alias jython_start="export PATH=$HOME/jython2.7.0/bin:$HOME/bin/apache-ant-1.10.5/bin:$PATH && export JAVA_HOME=`/usr/libexec/java_home` && export ANT_HOME=$HOME/bin/apache-ant-1.10.5 export CLASSPATH=$HOME/Code/gt/ml-cs7641/CS7641-machine-learning/assignment2/ABAGAIL/ABAGAIL.jar:$CLASSPATH"
