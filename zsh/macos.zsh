63c8dee8b110324878705758ac782c9e3596cb13
Patrick <patrick@patrickward.com>
2019-01-28 16:58:01 -0500

    Adding alias to show/hide hidden files in Finder

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -18,4 +18,13 @@ if [[ "$OSTYPE" == darwin* ]]; then
   # Pushes directory to the current Finder directory.
   alias pushdf='pushd "$(pfd)"'
 
+  # Show hidden files in Finder
+  alias showHiddenFiles='defaults write com.apple.finder AppleShowAllFiles YES; killall Finder /System/Library/CoreServices/Finder.app'
+
+  # Hide hidden files in Finder
+  alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
+
+  # Flush local DNS
+  alias flushDns='sudo killall -HUP mDNSResponder;say DNS cache has been flushed'
+
 fi
