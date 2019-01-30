c09c4f4af0e177332c02750549845907d3913ac2
Patrick <patrick@patrickward.com>
2019-01-30 12:09:19 -0500

    Adding alt docker starter for gios docker

--- zsh/gatech.zsh
+++ zsh/gatech.zsh
@@ -5,7 +5,11 @@ alias gt-docs="cd /Users/patrickward/Dropbox\ \(Personal\)/GT"
 
 # alias start_bonnie_docker="docker run --interactive --tty -v `pwd`:'/root/gios' -v '$HOME/.bonnie':'/root/.bonnie/' -w '/root/gios' --entrypoint=/bin/bash gtomscs/os"
 export GIOS_GRADER_DIR="$HOME/Code/gt/ios-cs6200/TA-ios-cs8803-02/gt-cs6200-graders"
-alias gt-start-bonnie-docker="docker run --interactive --tty -v $GIOS_GRADER_DIR:'/root/gios' -v '$HOME/.bonnie':'/root/.bonnie/' -w '/root/gios' --entrypoint=/bin/bash gtomscs/os"
+# alias gt-start-bonnie-docker="docker run --interactive --tty -v $GIOS_GRADER_DIR:'/root/gios' -v '$HOME/.bonnie':'/root/.bonnie/' -w '/root/gios' --entrypoint=/bin/bash gtomscs/os"
+alias gt-start-bonnie-docker="docker run --interactive --tty -v $GIOS_GRADER_DIR:'/root/gios' -v '$HOME/.bonnie':'/root/.bonnie/' -w '/root/gios' --entrypoint=/bin/bash fsgeek/omscs6200"
+
+export GRPC_EXPLORE_DIR="$HOME/Code/gt/ios-cs6200/grpc/grpc-exploration"
+alias gt-start-grpc-docker="docker run --interactive --tty -v $GRPC_EXPLORE_DIR:'/root/grpc' -w '/root/grpc' --entrypoint=/bin/bash fsgeek/omscs6200"
 
 # CS6475-CP
 # alias gt_cp_dir="cd $HOME/Code/gt/cp-cs6475"
