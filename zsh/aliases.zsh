f040a1afee2b3294a388f48ed08316055cd6bc1d
Patrick <patrick@patrickward.com>
2019-02-14 17:42:21 -0500

    Replacing todo aliases with functions

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -43,12 +43,26 @@ alias rmi='nocorrect rm -i'
 # todo-cli
 export TODOTXT_CONFIG='$HOME/.dotfiles/todo/todo.cfg'
 export TODOTXT_DEFAULT_ACTION=ls
-alias t="todo.sh -n -d $TODOTXT_CONFIG"
-alias tw="t ls +Work"
-alias tg="t ls +GaTech"
-alias th="t ls +Home"
-alias tl="t ls +Life"
-alias ts="t ls +Shopping"
+# alias t="todo.sh -n -d $TODOTXT_CONFIG"
+alias t="todo.sh -d $TODOTXT_CONFIG"
+
+todo_alias() {
+  if [[ -z "$1" ]]; then
+    t ls
+  elif [[ "$1" =~ "^(\+|\@).*" ]]; then
+    t ls $@
+  else
+    [[ ! -z "$1" ]] && t $@
+  fi
+}
+
+# alias tw="t ls +Work"
+tw() { todo_alias $@ "+Work" }
+twnext() { todo_alias $@ "+Work" "@next" "-@assigned" }
+tg() { todo_alias $@ "+GaTech" }
+th() { todo_alias $@ "+Home" }
+tl() { todo_alias $@ "+Life" }
+ts() { todo_alias $@ "+Shopping" }
 
 # GTM and Git
 alias gtpush='git push && git pushgtm'
