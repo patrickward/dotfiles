00093f5d348bb4d9e13c869496e6cc226be6fa75
Patrick <patrick@patrickward.com>
2019-02-21 08:19:01 -0500

    Updating dns flush command

--- zsh/macos.zsh
+++ zsh/macos.zsh
@@ -21,7 +21,7 @@ if [[ "$OSTYPE" == darwin* ]]; then
   alias hideHiddenFiles='defaults write com.apple.finder AppleShowAllFiles NO; killall Finder /System/Library/CoreServices/Finder.app'
 
   # Flush local DNS
-  alias flushDns='sudo killall -HUP mDNSResponder;say DNS cache has been flushed'
+  alias flushDns='sudo dscacheutil -flushcache && sudo killall -HUP mDNSResponder && echo "DNS cache has been flushed"'
 
   # Make sure screenshots go to the Downloads folder
   alias screenshotsToDownloads='defaults write com.apple.screencapture location ~/Downloads/ && killall SystemUIServer'
