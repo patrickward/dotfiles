3cf8dde5a3f90ab9fc7840338e6d7c37c4bf9ff5
Patrick Ward <patrick@octetic.com>
2024-08-12 05:42:38 -0400

    updating to toml config

--- src/alacritty/setup.sh
+++ src/alacritty/setup.sh
@@ -1,3 +1,3 @@
 . "$DOTFILES/lib/utils.sh"
 mkdir -p "$XDG_CONFIG_HOME/alacritty"
-link_file "${DOTFILES}/src/alacritty/alacritty.yml" "$XDG_CONFIG_HOME/alacritty/alacritty.yml"
+link_file "${DOTFILES}/src/alacritty/alacritty.toml" "$XDG_CONFIG_HOME/alacritty/alacritty.toml"
