26cc78fab995751b21ca03cf47f041f93fac486a
patrickward <patrickward@users.noreply.github.com>
2025-12-13 17:33:53 -0500

    Removing nvm - too slow

--- src/nvm/path.zsh
+++ src/nvm/path.zsh
@@ -1,7 +1,10 @@
-
-export NVM_DIR="$HOME/.config/nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# Uncomment the following three lines to enable nvm
+# Removed, because it's slow to load on shell startup.
+# -- BEGIN --
+# export NVM_DIR="$HOME/.config/nvm"
+# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+# -- END --
 
 # Automatically call nvm use whenever you enter a directory that contains a .nvmrc file
 # This must be placed after nvm initialization!
@@ -9,15 +12,15 @@ export NVM_DIR="$HOME/.config/nvm"
 # Uncomment the following to autoload
 # -- BEGIN --
 # autoload -U add-zsh-hook
-# 
+#
 # load-nvmrc() {
 #   local nvmrc_path
 #   nvmrc_path="$(nvm_find_nvmrc)"
-# 
+#
 #   if [ -n "$nvmrc_path" ]; then
 #     local nvmrc_node_version
 #     nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-# 
+#
 #     if [ "$nvmrc_node_version" = "N/A" ]; then
 #       nvm install
 #     elif [ "$nvmrc_node_version" != "$(nvm version)" ]; then
@@ -28,7 +31,7 @@ export NVM_DIR="$HOME/.config/nvm"
 #     nvm use default
 #   fi
 # }
-# 
+#
 # add-zsh-hook chpwd load-nvmrc
 # load-nvmrc
 # -- END --
