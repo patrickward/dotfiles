6b64e49c2e22d404d476a90d32a132dd8beda1f5
patrickward <patrickward@users.noreply.github.com>
2025-12-13 16:13:06 -0500

    Removing zk

--- src/zk/setup.sh
+++
@@ -1,8 +0,0 @@
-. "$DOTFILES/lib/utils.sh"
-mkdir -p "$XDG_CONFIG_HOME/zk"
-mkdir -p "$HOME/notes/daily"
-mkdir -p "$HOME/notes/standup"
-mkdir -p "$HOME/notes/projects"
-mkdir -p "$HOME/notes/roadtrips"
-link_file "${DOTFILES}/src/zk/config.toml" "$XDG_CONFIG_HOME/zk/config.toml"
-link_file "${DOTFILES}/src/zk/templates" "$XDG_CONFIG_HOME/zk/templates"
