d71698e740b8282e5a4fbd634e196d9eb727c574
patrickward <patrickward@users.noreply.github.com>
2026-08-24 17:02:21 -0400

    feat(ghostty): add ghostty config and setup.sh

---
+++ topics/ghostty/setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env zsh
+# topics/ghostty/setup.sh
+#
+# Symlinks the ghostty config into ~/.config/ghostty/.
+# Sourced by `zdot setup`, which provides link_file and $DOTFILES.
+
+mkdir -p "$XDG_CONFIG_HOME/ghostty"
+link_file "$DOTFILES/topics/ghostty/config" "$XDG_CONFIG_HOME/ghostty/config"
